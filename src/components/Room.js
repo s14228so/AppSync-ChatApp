43d1590a2ac477c75fb02aeb3d270e9be5502a66
s14228so <saongtx7@gmail.com>
2020-02-24 22:48:33 -1000

    messages取得用のquery 作成

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AddMessage from "./AddMessage"
+import { API, graphqlOperation } from 'aws-amplify';
+
+const getRoom = `query getRoom($id: ID!) {
+  getRoom(id: $id) {
+    id
+    name
+    messages {
+      items {
+        id
+        title
+        username
+      }
+      nextToken
+    }
+  }
+}
+`;
+
+
 export default ({ room }) => {
+
+  const [roomData, setRoomData] = useState(room)
+
+  useEffect(() => {
+    const fetchRoomData = async () => {
+      const input = { id: room.id };
+      const res = await API.graphql(graphqlOperation(getRoom, input));
+      setRoomData(res.data.getRoom)
+    }
+    fetchRoomData()
+
+  }, [])
   const renderRoom = () => {
     if (room.messages.length) {
       return (
